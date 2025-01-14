19c7b0ebd7a049ec073e696e43d21e4505eaa853
Marco Braga <maboa17@gmail.com>
2025-01-14 17:54:03 -0300

    Update configuration file: .eslintrc.js

--- user-ms/.eslintrc.js
+++ user-ms/.eslintrc.js
@@ -5,11 +5,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ["prettier"],
+  extends: ["airbnb-base", "prettier"],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "no-console": ["error", { allow: ["warn", "error", "log"] }],
   },
 };
