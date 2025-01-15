793e92254490ac45624fac6b3a9c37488cadaff5
Marco Braga <maboa17@gmail.com>
2025-01-15 18:40:42 -0300

    Add dependencies: class-validator and class-transformer

--- order-ms/src/order/create-order.dto.ts
+++ order-ms/src/order/create-order.dto.ts
@@ -1,11 +1,16 @@
+import { IsString, IsNumber } from 'class-validator';
 
 export class CreateOrderDto {
+    @IsString()
     readonly productName: string;
 
+    @IsString()
     readonly price: number;
 
+    @IsNumber()
     readonly quantity: number;
     
+    @IsString()
     readonly status: string;
 
 }
