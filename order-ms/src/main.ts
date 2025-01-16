f5be99dc2670d161a036cb9597a9e2c39804520e
Marco Braga <maboa17@gmail.com>
2025-01-16 00:01:35 -0300

    Set up Swagger in main file

--- order-ms/src/main.ts
+++ order-ms/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Order Service API')
+    .setDescription('API documentation for the Order Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log(`Order Service is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- user-ms/src/main.ts
+++ user-ms/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('User Service API')
+    .setDescription('API documentation for the User Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+  console.log(`User Service is running on: ${await app.getUrl()}`);
 }
 bootstrap();
